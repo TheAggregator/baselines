b64910dd654753e47412ece648a37eaf6fa55106
liam <liamyafuso@gmail.com>
2018-04-29 18:00:39 -0700

    adds resnet policy for ppo

--- baselines/ppo2/policies.py
+++ baselines/ppo2/policies.py
@@ -15,6 +15,24 @@ def nature_cnn(unscaled_images):
     h3 = conv_to_fc(h3)
     return activ(fc(h3, 'fc1', nh=512, init_scale=np.sqrt(2)))
 
+def resnet_cnn(unscaled_images):
+    """
+    CNN with resnet blocks.
+    """
+    scaled_images = tf.cast(unscaled_images, tf.float32) / 255.
+    activ = tf.nn.relu
+    r1_1 = activ(conv(scaled_images, 'r1_1', nf=32, rf=8, stride=1, init_scale=np.sqrt(2)))
+    r1_2 = activ(conv(h1, 'r1_2', nf=32, rf=8, stride=1, init_scale=np.sqrt(2)))
+    r1_3 = activ(conv(tf.concat(3, [h1, h2]), 'r1_3', nf=32, rf=8, stride=4, init_scale=np.sqrt(2)))
+
+    r2_1 = activ(conv(r1_3, 'r2_1', nf=64, rf=4, stride=1, init_scale=np.sqrt(2)))
+    r2_2 = activ(conv(r2_1, 'r2_2', nf=64, rf=4, stride=1, init_scale=np.sqrt(2)))
+    r2_3 = activ(conv(tf.concat(3, [r2_1, r2_2]), 'r2_3', nf=64, rf=4, stride=2, init_scale=np.sqrt(2)))
+
+    h = activ(conv(r2_3, 'h', nf=64, rf=3, stride=1, init_scale=np.sqrt(2)))
+    h = conv_to_fc(h)
+    return activ(fc(h, 'fc1', nh=512, init_scale=np.sqrt(2)))
+
 class LnLstmPolicy(object):
     def __init__(self, sess, ob_space, ac_space, nbatch, nsteps, nlstm=256, reuse=False):
         nenv = nbatch // nsteps
@@ -149,6 +167,38 @@ class MlpPolicy(object):
 
         self.pdtype = make_pdtype(ac_space)
         self.pd = self.pdtype.pdfromflat(pdparam)
+
+        a0 = self.pd.sample()
+        neglogp0 = self.pd.neglogp(a0)
+        self.initial_state = None
+
+        def step(ob, *_args, **_kwargs):
+            a, v, neglogp = sess.run([a0, vf, neglogp0], {X:ob})
+            return a, v, self.initial_state, neglogp
+
+        def value(ob, *_args, **_kwargs):
+            return sess.run(vf, {X:ob})
+
+        self.X = X
+        self.pi = pi
+        self.vf = vf
+        self.step = step
+        self.value = value
+
+class ResnetPolicy(object):
+
+    def __init__(self, sess, ob_space, ac_space, nbatch, nsteps, reuse=False): #pylint: disable=W0613
+        nh, nw, nc = ob_space.shape
+        ob_shape = (nbatch, nh, nw, nc)
+        nact = ac_space.n
+        X = tf.placeholder(tf.uint8, ob_shape) #obs
+        with tf.variable_scope("model", reuse=reuse):
+            h = resnet_cnn(X)
+            pi = fc(h, 'pi', nact, init_scale=0.01)
+            vf = fc(h, 'v', 1)[:,0]
+
+        self.pdtype = make_pdtype(ac_space)
+        self.pd = self.pdtype.pdfromflat(pi)
 
         a0 = self.pd.sample()
         neglogp0 = self.pd.neglogp(a0)
