03a118e82c6f3a98290507e3403216fde6e5a95f
liam <liamyafuso@gmail.com>
2018-05-03 17:44:58 -0700

    loggin in dummyvecenv

--- baselines/common/vec_env/dummy_vec_env.py
+++ baselines/common/vec_env/dummy_vec_env.py
@@ -5,8 +5,10 @@ from . import VecEnv
 class DummyVecEnv(VecEnv):
     def __init__(self, env_fns):
         self.envs = [fn() for fn in env_fns]
+        print("Environments created...")
         env = self.envs[0]
         VecEnv.__init__(self, len(env_fns), env.observation_space, env.action_space)
+        print("DummyVec init supered...")
 
         obs_spaces = self.observation_space.spaces if isinstance(self.observation_space, gym.spaces.Tuple) else (self.observation_space,)
         self.buf_obs = [np.zeros((self.num_envs,) + tuple(s.shape), s.dtype) for s in obs_spaces]
